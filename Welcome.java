6192fecc4a8cb30c65fe4b0544a1682918910398
Yash <yashatkulwar2000@gmail.com>
2022-08-14 18:01:12 +0530

    added welcome message

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package Day8LineComparison_Welcome;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
